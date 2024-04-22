43ab7b2501b83843800fa9139cffbc0ae8579129
Kirill Ivashkov <kirillivashk@gmail.com>
2024-04-22 11:31:13 +0300

    update: add db.json and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Person from './components/Person';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchPerson, setSearchPerson] = useState('');
 
   const filteredPersons = persons.filter(
     (person) => person.name.toLowerCase().indexOf(searchPerson.toLowerCase()) > -1,
   );
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log(response);
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
@@ -24,7 +28,7 @@ const App = () => {
       <PersonForm persons={filteredPersons} setPersons={setPersons} />
       <h2>Numbers</h2>
       {filteredPersons.map((person) => (
-        <Person key={person.name} name={person.name} number={person.number} />
+        <Person key={person.id} name={person.name} number={person.number} />
       ))}
     </div>
   );
